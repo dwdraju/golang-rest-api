38aaf6f6b3bd5380785e96b9f9e75f3dbba1dcb2
dwdraju <rajudawadinp@gmail.com>
2023-01-25 08:24:42 +0000

    add route for dump vars

--- api/main.go
+++ api/main.go
@@ -2,82 +2,109 @@ package main
 
 import (
     "encoding/json"
+    "fmt"
     "github.com/gorilla/mux"
     "log"
     "net/http"
-    "fmt"
+    "os"
 )
 
 type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
+    ID string `json:"id,omitempty"`
+    Firstname string `json:"firstname,omitempty"`
+    Lastname string `json:"lastname,omitempty"`
+    Address * Address `json:"address,omitempty"`
 }
 type Address struct {
-    City  string `json:"city,omitempty"`
+    City string `json:"city,omitempty"`
     State string `json:"state,omitempty"`
 }
 
-var people []Person
+var people[] Person
 
-func HomePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w,"Hello there")
+func HomePage(w http.ResponseWriter, r * http.Request) {
+    fmt.Fprintf(w, "Hello there")
 }
 
 // Add health check
-func HealthCheck(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w,"Health Check Pass !!!")
+func HealthCheck(w http.ResponseWriter, r * http.Request) {
+    fmt.Fprintf(w, "Health Check Pass !!!")
 }
 
 // Display all from the people var
-func GetPeople(w http.ResponseWriter, r *http.Request) {
+func GetPeople(w http.ResponseWriter, r * http.Request) {
     json.NewEncoder(w).Encode(people)
 }
 
 // Display a single data
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for _, item := range people {
+func GetPerson(w http.ResponseWriter, r * http.Request) {
+    params: = mux.Vars(r)
+    for _,
+    item: = range people {
         if item.ID == params["id"] {
             json.NewEncoder(w).Encode(item)
             return
         }
     }
-    json.NewEncoder(w).Encode(&Person{})
+    json.NewEncoder(w).Encode( & Person {})
 }
 
 // create a new item
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
+func CreatePerson(w http.ResponseWriter, r * http.Request) {
+    params: = mux.Vars(r)
     var person Person
-    _ = json.NewDecoder(r.Body).Decode(&person)
+    _ = json.NewDecoder(r.Body).Decode( & person)
     person.ID = params["id"]
     people = append(people, person)
     json.NewEncoder(w).Encode(people)
 }
 
 // Delete an item
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for index, item := range people {
+func DeletePerson(w http.ResponseWriter, r * http.Request) {
+    params: = mux.Vars(r)
+    for index,
+    item: = range people {
         if item.ID == params["id"] {
-            people = append(people[:index], people[index+1:]...)
+            people = append(people[: index], people[index + 1: ]...)
             break
         }
         json.NewEncoder(w).Encode(people)
     }
 }
 
+// Dump vars for debugging
+func DumpVars(w http.ResponseWriter, r * http.Request) {
+    for _, e: = range os.Environ() {
+        fmt.Fprintf(w, e)
+    }
+}
+
 // main function to boot up everything
 func main() {
-    router := mux.NewRouter()
-    port   := 8090
-    addr := fmt.Sprintf(":%d", port)
-    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+    router: = mux.NewRouter()
+    port: = 8080
+    addr: = fmt.Sprintf(":%d", port)
+    people = append(people, Person {
+        ID: "1",
+        Firstname: "John",
+        Lastname: "Doe",
+        Address: & Address {
+            City: "City X",
+            State: "State X"
+        }
+    })
+    people = append(people, Person {
+        ID: "2",
+        Firstname: "Koko",
+        Lastname: "Doe",
+        Address: & Address {
+            City: "City Z",
+            State: "State Y"
+        }
+    })
     router.HandleFunc("/", HomePage).Methods("GET")
     router.HandleFunc("/healthz", HealthCheck).Methods("GET")
+    router.HandleFunc("/dumpvars", DumpVars).Methods("GET")
     router.HandleFunc("/people", GetPeople).Methods("GET")
     router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
     router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
